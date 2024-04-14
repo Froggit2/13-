c8c82149df4f6f49aba71b3b6c8747f11d2ac34b
Froggit2 <165292357+Froggit2@users.noreply.github.com>
2024-04-14 11:18:44 +0300

    Delete While.py

--- While.py
+++
@@ -1,9 +0,0 @@
-while 1 > 0 or True:
-    number = int(input('Введи число: '))
-    if number % 2 == 0:
-        print(number, 'Четное число')
-        if number % 5 == 0:
-            print('Конец')
-            break
-    if number % 2 != 0:
-        print(number, 'НЕ четное')
